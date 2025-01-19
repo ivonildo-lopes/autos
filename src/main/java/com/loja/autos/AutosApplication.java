ea20877883dcf43be034ef83d217c84dc3ca9250
ivonildolopes <ivonildolopes@goldencloud.tech>
2025-01-19 02:05:05 -0300

    exemplo basico de job com schedule

--- src/main/java/com/loja/autos/AutosApplication.java
+++ src/main/java/com/loja/autos/AutosApplication.java
@@ -2,8 +2,10 @@ package com.loja.autos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AutosApplication {
 
 	public static void main(String[] args) {
